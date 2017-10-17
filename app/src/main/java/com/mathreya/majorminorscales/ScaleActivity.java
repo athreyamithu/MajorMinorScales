7789ff75db92a505fa982ca5cb86704524da3d8c
athreyamithu <mithuathreya@gmail.com>
2017-10-17 00:05:16 -0700

    Successfully implemented Android wrapper for Spotify API (thanks kaees), changed UI and design for both activities, modified JSON file

--- app/src/main/java/com/mathreya/majorminorscales/ScaleActivity.java
+++ app/src/main/java/com/mathreya/majorminorscales/ScaleActivity.java
@@ -8,32 +8,122 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.InputStream;
-import java.util.HashMap;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyCallback;
+import kaaes.spotify.webapi.android.SpotifyError;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Track;
+
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import android.os.StrictMode;
 
 public class ScaleActivity extends AppCompatActivity {
     ProgressDialog mProgressDialog;
+    private OkHttpClient client = new OkHttpClient();
+    private static final String CLIENT_ID = "4d62d8c89a1b49f58b2f75caca7ffb53";
+    private static final String REDIRECT_URI = "major-minor-login://callback";
+    private static final int REQUEST_CODE = 1997;
+    private String tonic;
+    private String scale;
+    private String sc;
+    private String json;
+    private String access_token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_scale);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
+        tonic = bundle.getString("Tonic");
+        scale = bundle.getString("Scale");
         TextView scaleHeader = (TextView) findViewById(R.id.scale_header);
-        scaleHeader.setText(bundle.getString("Tonic") + " " + bundle.getString("Scale"));
-        new ScaleNotes().execute(bundle.getString("Tonic"), bundle.getString("Scale").toLowerCase().replace(" ", ""));
+        scaleHeader.setText(tonic + " " + scale);
+        new ScaleNotes().execute(tonic, scale.toLowerCase().replace(" ", ""));
+        sc = (scale.equals("Major")) ? "major" : "minor";
+        login();
+
+    }
+
+    private void login() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    public void readJSONStream() {
+        try {
+            InputStream is = getAssets().open("scales.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            //if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                access_token = response.getAccessToken();
+                Log.i("ScaleActivity", access_token);
+            //}
+        }
+        createSpotifyPreview(tonic, sc, access_token);
+    }
+
+    public void createSpotifyPreview(String tonic, String scale, String access_token) {
+        try {
+            readJSONStream();
+            JSONObject obj = new JSONObject(json);
+            String id = obj.getJSONObject(tonic).getString(scale + "spotifyids");
+            String songtitle = obj.getJSONObject(tonic).getJSONArray(scale + "popularsongs").get(0).toString();
+            TextView songtext = (TextView) findViewById(R.id.song_title);
+            songtext.setText(songtitle);
+            SpotifyApi api = new SpotifyApi();
+            api.setAccessToken(access_token);
+            SpotifyService service = api.getService();
+            Track track = service.getTrack(id);
+//            Headers mAuthHeader = Headers.of("Authorization", "Bearer " + access_token);
+//            Request request = new Request.Builder()
+//                    .get()
+//                    .url("https://api.spotify.com/v1/tracks/" + id)
+//                    .headers(mAuthHeader)
+//                    .build();
+//            Response response = client.newCall(request).execute();
+//            JSONObject res = new JSONObject(response.body().string());
+//            Log.i("ScaleActivity Spotify", res.getString("preview_url"));
+            String preview = track.preview_url;
+            Log.i(songtitle, preview);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private class ScaleNotes extends AsyncTask<String, Void, Void> {
@@ -44,8 +134,7 @@ public class ScaleActivity extends AppCompatActivity {
         protected void onPreExecute() {
             super.onPreExecute();
             mProgressDialog = new ProgressDialog(ScaleActivity.this);
-            mProgressDialog.setTitle("Major Scales");
-            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setTitle("Loading...");
             mProgressDialog.setIndeterminate(false);
             mProgressDialog.show();
         }
