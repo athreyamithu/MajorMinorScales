fbe6617f66ed8936b29ba159a7fe59e20b79356a
athreyamithu <mithuathreya@gmail.com>
2018-02-07 18:16:43 -0800

    Updated Welcome Activity, saving before updating Android Studio

--- app/src/main/java/com/mathreya/majorminorscales/WelcomeActivity.java
+++ app/src/main/java/com/mathreya/majorminorscales/WelcomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +18,6 @@ import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
     private ViewPager viewPager;
-
     private LinearLayout layoutDots;
     private TextView[] dots;
     private int[] layouts;
@@ -33,18 +33,53 @@ public class WelcomeActivity extends AppCompatActivity {
                 R.layout.activity_welcome_page2,
                 R.layout.activity_welcome_page3 };
         //addBottomDots(0);
+        viewPager.setAdapter(new WelcomeActivityAdapter());
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                //addBottomDots(position);
+            }
 
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {}
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {}
+        });
     }
 
-//    public class WelcomeActivityAdapter extends PagerAdapter {
-//        private LayoutInflater layoutInflater;
-//
-//        @Override
-//        public Object instantiateItem(ViewGroup container, int position) {
-//            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//            View view = layoutInflater.inflate(layouts[position], container, false);
-//            container.addView(view);
-//            return view;
-//        }
-//    }
+    private void addBottomDots(int currentPage) {
+        TextView[] dots = new TextView[layouts.length];
+        int[] activeDots = getResources().getIntArray(R.array.array_dot_active),
+         inactiveDots = getResources().getIntArray(R.array.array_dot_inactive);
+        layoutDots.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            //dots[i].setText(Html.fromHtml("&#8226;", Html.FROM_HTML_MODE_LEGACY));
+        }
+    }
+
+    public class WelcomeActivityAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) { return view == obj; }
+
+        @Override
+        public int getCount() { return layouts.length; }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
 }
